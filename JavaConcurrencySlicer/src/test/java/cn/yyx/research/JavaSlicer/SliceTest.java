b14c72a8acbdd08fb9e03d7ac05e722649511590
DELL <DELL@DESKTOP-20DEPKI>
2016-12-23 12:31:37 +0800

    small change.

--- JavaConcurrencySlicer/src/test/java/cn/yyx/research/JavaSlicer/SliceTest.java
+++ JavaConcurrencySlicer/src/test/java/cn/yyx/research/JavaSlicer/SliceTest.java
@@ -1,20 +1,9 @@
 package cn.yyx.research.JavaSlicer;
 
-import java.io.File;
-import java.util.Iterator;
-import java.util.List;
-
-import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jface.text.Document;
 import org.junit.Assert;
 import org.junit.Test;
 
-import cn.yyx.research.file_util.FileUtil;
-import cn.yyx.research.java_slicer.TestCase;
-import cn.yyx.research.jdt_helper.JDT_Util;
 import cn.yyx.research.slice.Slicer;
-import cn.yyx.research.slice_visitor.ParseSliceVisitor;
-import cn.yyx.research.slice_visitor.util.SlicedCodeGenerator;
 
 public class SliceTest
 {
